970710e6421081c1d5f9020058ee6be6aae42734
Jecksan Xavier <jecksanx@gmail.com>
2025-11-24 06:39:01 -0600

    Delete recibo2.0 directory

--- recibo2.0/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "recibo_de_dinero");
-
-if ($conexion->connect_errno) {
-    echo "Error al conectar: " . $conexion->connect_error;
-    exit();
-}
-?>
--- recibo2.0/eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("db.php");
-
-$id = $_GET['id'];
-
-$conexion->query("DELETE FROM recibos WHERE id=$id");
-
-header("Location: index.php");
-?>
